e171817a712828fb861c3faa72cf2394948d081b
rboindala <51992474+rboindala@users.noreply.github.com>
2020-08-01 10:46:06 +0530

    Add files via upload

---
+++ Rprogramming/ASSIGNMENT 1 PT 1.R
@@ -0,0 +1,13 @@
+pollutanmean<-function(directory,pollutant,id=1:332){
+  filelist<-list.files(path=directory, pattern = ".csv",full.names = TRUE)
+  values<-numeric()
+  for(i in id){
+    data<-read.csv(filelist[i])
+    values<-c(values,data[[pollutant]])
+  }
+  mean(values,na.rm = TRUE)
+  
+}
+
+pollutanmean("C:\Users\brith\OneDrive\Desktop\specdata","sulfate")
+
